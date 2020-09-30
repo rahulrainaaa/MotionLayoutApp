8b38626bff01314b29012ba2f37af93cc2d06b8e
rahul.raina <rahul.raina@ril.com>
2020-09-30 17:34:18 +0530

    Some Code Commit.

--- app/src/main/java/com/example/motionlayoutapp/MainActivity.kt
+++ app/src/main/java/com/example/motionlayoutapp/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_a)
-
+        setContentView(R.layout.layout_dashboard)
     }
 }
